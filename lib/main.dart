eaa85aedffbb94e4f9cacfbafb81255b053fadf7
Benedict <lliam.benedict@gmail.com>
2026-02-04 22:50:11 +0800

    main app structure and theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const GradeCalculatorApp());
 }
+
+class GradeCalculatorApp extends StatelessWidget {
+  const GradeCalculatorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Student Grade Calculator',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
+        fontFamily: 'Roboto',
+      ),
+      home: const GradeHomePage(),
+    );
+  }
+}
